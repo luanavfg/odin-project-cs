50584a1fa54226aa12eca5fd27122a3a86cb6e9e
luanavfg <luanavefago1@gmail.com>
2025-07-08 19:50:44 -0300

    feat: add mergeSort

--- mergeSort.js
+++ mergeSort.js
@@ -1,35 +1,29 @@
-// [9, 2, 5, 13, 8, 5, 0, 1]
-
 function mergeSort(array) {
   const arrayLength = array.length
   
-  var smallerNum
-  var biggestNum
-  if (arrayLength === 2) {
-    smallerNum = array[0] < array[1] ? array[0] : array[1]
-    biggestNum = array[0] < array[1] ? array[1] : array[0]
-    
-    return [smallerNum, biggestNum]
+  if (array.length ===1) {
+    return [array[0]]
   }
 
-  // {smallerNum: [smallerNum], biggestNum: [biggestNum]}
-  const left = mergeSort(array.slice(0, arrayLength/2)) // [[2],[9]] [2,9] [2]
-  console.log('left', left)
+  const left = mergeSort(array.slice(0, arrayLength/2)) 
+  const right = mergeSort(array.slice(arrayLength/2, arrayLength))
+
+  const result = [];
+  let i = 0;
+  let j = 0;
   
-  const right = mergeSort(array.slice(arrayLength/2, arrayLength)) // [[5],[13]] [5,9]
-  console.log('right', right)
-
-  const newLeft = left.pop()
-  // console.log('newLeft', newLeft)
-
-  const newRight = right.pop()
-  // console.log('newRight', newRight)
-
-  const result = [mergeSort([...left[0], ...right[0]]), mergeSort([...newLeft, ...newRight])]
-  
-  // console.log(result)
-  return result
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      result.push(left[i]);
+      i++;
+    } else {
+      result.push(right[j]);
+      j++;
+    }
+  }
+  return result.concat(left.slice(i)).concat(right.slice(j));
 }
-const myArray = [9, 2, 5, 13, 8, 5, 0, 1]
-const x = mergeSort(myArray)
-console.log(x)
+
+const myArray =   [9, 2, 5, 13, 8, 4, 0, 1]
+const finalMerge = mergeSort(myArray)
+console.log(finalMerge)
